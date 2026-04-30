3d6fd52f9e100d3e16f786c0be4a35a204843c7d
fabri98bazan <fabri98201098@gmail.com>
2026-04-30 18:50:24 -0300

    asitenfis de 30 de abri Bazan Fabricio

--- src/js/ejercicio20(asistencia).js
+++ src/js/ejercicio20(asistencia).js
@@ -1,2 +1,3 @@
 import { servicioGiselaCarlos } from "../../services/serviceCarlosGisela";
 import {serviceTapiaCarlosMatias} from "/services/serviceTapiaCarlosMatias.js";
+import { serviceBazanFabricio } from "../../services/serviceBazanFabricio";
