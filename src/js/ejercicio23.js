57d28bbf9c5ce88b543d611138d37572367eda60
Carlos Matias Tapia <matiastrimax18@gmail.com>
2026-05-02 02:27:50 -0300

    Tapia Carlos Matias - Desarrollo del ejercicio 24 y correcciones del ejercicio 23

--- src/js/ejercicio23.js
+++ src/js/ejercicio23.js
@@ -1,19 +1,17 @@
 import {textoLargo} from "../../services/serviceEjercicio23.js";
 
-const ingresoTexto = document.querySelector("#inputTexto");
-const salidaTexto = document.querySelector("#ouputTexto");
+const ingresoTexto = document.querySelector('#inputTexto');
+const salidaTexto = document.querySelector('#ouputTexto');
 
-ingresoTexto.addEventListener("input", (evento) => {
+ingresoTexto.addEventListener('input', (evento) => {
     const valorTexto = evento.target.value;
 
     salidaTexto.textContent = valorTexto || "Esperando el texto...";
 
     if(textoLargo(valorTexto)){
-        salidaTexto.style.backgroundColor = "#d57373";
-        salidaTexto.style.borderColor = "#ffffff";
+        salidaTexto.classList.add('fondoTextoLargo');
     }
     else{
-        salidaTexto.style.backgroundColor = "#90c964";
-        salidaTexto.style.borderColor = "#ffffff";
+        salidaTexto.classList.remove('fondoTextoLargo');
     }
 });
---
+++ src/js/ejercicio24.js
@@ -0,0 +1,33 @@
+import {descripcionStack} from '../../services/serviceEjercicio24.js';
+
+// Botones
+const botonStack = document.querySelectorAll('input[name="stack"]');
+const botonTema = document.querySelectorAll('input[name="tema"]');
+const resultadoStack = document.querySelector('#mostrarStack');
+
+// Change Tema Visual
+botonTema.forEach(boton => {
+    boton.addEventListener('change', (evento) => {
+        if (evento.target.value === 'oscuro') {
+            document.body.classList.add('modo-oscuro');
+        } else {
+            document.body.classList.remove('modo-oscuro');
+        }
+
+        // Resultado por consola
+        console.log(`Tema cambiado a: ${evento.target.value}`);
+    });
+});
+
+// Change Stack
+botonStack.forEach(boton => {
+    boton.addEventListener('change', (evento) => {
+        const seleccion = evento.target.value;
+        const info = descripcionStack(seleccion);
+        
+        resultadoStack.textContent = info;
+
+        // Resultado por consola
+        console.log(`Stack seleccionado: ${seleccion}`);
+    });
+});
